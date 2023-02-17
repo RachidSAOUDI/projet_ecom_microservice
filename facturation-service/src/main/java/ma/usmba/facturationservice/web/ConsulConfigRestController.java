f23e3843494687ef6235f07a044934ad757a7d00
RachidSAOUDI <rachid.saoudi@usmba.ac.ma>
2023-02-17 15:59:56 +0100

    MAj fact-service

--- facturation-service/src/main/java/ma/usmba/facturationservice/web/ConsulConfigRestController.java
+++ facturation-service/src/main/java/ma/usmba/facturationservice/web/ConsulConfigRestController.java
@@ -2,20 +2,27 @@ package ma.usmba.facturationservice.web;
 
 import ma.usmba.facturationservice.config.MyConsulConfig;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class ConsulConfigRestController {
     @Autowired
     private MyConsulConfig myConsulConfig;
-//    @Value("${token.accessTokenTimeout}")
-//    private long accessTokenTimeout;
-//    @Value("${token.refreshTokenTimeout}")
-//    private long refreshTokenTimeout;
+    @Value("${token.accessTokenTimeout}")
+    private long accessTokenTimeout;
+    @Value("${token.refreshTokenTimeout}")
+    private long refreshTokenTimeout;
 
+//    @GetMapping("/myConfig")
+//    public MyConsulConfig myConfig(){
+//        return myConsulConfig;
+//    }
     @GetMapping("/myConfig")
-    public MyConsulConfig myConfig(){
-        return myConsulConfig;
+    public Map<String, Object> myConfig(){
+        return Map.of("accessTokenTimeout",accessTokenTimeout,"refreshTokenTimeout",refreshTokenTimeout);
     }
 }
